8f90a3f47798b85520a0fc427bfcc927dc4cd289
monakilsgard <31917439+monakilsgard@users.noreply.github.com>
2018-09-29 00:56:53 +0200

    Adding a Unique ID generator class and all its functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("hej");
-    }
+    //public static void main(String[] args){
+      //  System.out.println("hej");
+
+
+    //}
 
 }
