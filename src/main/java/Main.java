2300e819369cff37199c4c97961eacfa91eb56bb
monakilsgard <31917439+monakilsgard@users.noreply.github.com>
2018-10-09 15:26:51 +0200

    Changes display name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("Hej");
+        stage.setTitle("Yellow");
         stage.setScene(scene);
         stage.show();
     }
