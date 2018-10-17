be6e7ae39fbb5eb971d323ae2de6395da6f6c69c
Viktor Valadi <viktor.valadi@hotmail.se>
2018-10-17 14:55:08 +0200

    readme is complete

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -9,7 +9,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.*;
 
